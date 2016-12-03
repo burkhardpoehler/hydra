7edec22b5a5dfdfbfd988289fd75a2680915dc37
Poehler <burkhard.poehler@googlemail.com>
2016-12-03 15:49:59 +0100

    My second

--- com.hydra.project/src/com/hydra/project/exceptions/testclass.java
+++ com.hydra.project/src/com/hydra/project/exceptions/testclass.java
@@ -3,6 +3,8 @@ package com.hydra.project.exceptions;
 public class testclass {
 	public static void main(String[] args){
 		System.out.println("hello world");
+		//comment
+		System.out.println("hello world");
 	}
 
 	
